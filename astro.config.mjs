c50c7313e069f3406ffd745d6b81d355c22dcc4d
torbatti <torbattii@gmail.com>
2025-02-14 22:58:49 +0330

    init

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://0xab.ir',
+  integrations: [sitemap()]
+});
